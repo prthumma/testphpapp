bb0008034e6cc4f2f75eb6e0b8b97a53c7651a98
Prabhakar <preddy@reisystems.com>
2015-07-27 18:03:31 -0400

    update code

--- src/processFO.php
+++ src/processFO.php
@@ -19,7 +19,11 @@
 $xmlFile = ($_SERVER['DOCUMENT_ROOT'] . '/files/working/GrantsDBExtract20150727.xml');
 try{
   // Open the XML
-  echo ('TEST FROM HEROKU' . $_SERVER['DOCUMENT_ROOT']); return;
+  echo ('TEST FROM HEROKU' . $_SERVER['DOCUMENT_ROOT']);
+
+  print_r(get_loaded_extensions());
+
+  return;
 
   $handle = fopen($xmlFile, 'r');
 
